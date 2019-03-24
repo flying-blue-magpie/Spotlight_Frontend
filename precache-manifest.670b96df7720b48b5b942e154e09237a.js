c697f1527eb36bf47e6b8ae84f4474583a5c811f
wanlinlin <leo12345639@gmail.com>
2019-03-24 21:43:40 +0800

    Updates

---
+++ precache-manifest.670b96df7720b48b5b942e154e09237a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98b3ae47f9419c7c190",
+    "url": "/Spotlight_Frontend/static/css/main.31f1298e.chunk.css"
+  },
+  {
+    "revision": "a98b3ae47f9419c7c190",
+    "url": "/Spotlight_Frontend/static/js/main.58c5b2cb.chunk.js"
+  },
+  {
+    "revision": "9449ac1b54603c2bc090",
+    "url": "/Spotlight_Frontend/static/js/runtime~main.e6f4d450.js"
+  },
+  {
+    "revision": "86223985acd85dd96bec",
+    "url": "/Spotlight_Frontend/static/js/2.9ad3f6bd.chunk.js"
+  },
+  {
+    "revision": "a134f0907e1cb95134f17b4c10d10b8f",
+    "url": "/Spotlight_Frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Spotlight_Frontend/precache-manifest.e21f0a3034b8d4f3fd6c9d99d01fb08c.js"
+  "/Spotlight_Frontend/precache-manifest.670b96df7720b48b5b942e154e09237a.js"
 );
 
 workbox.clientsClaim();
