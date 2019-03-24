972130ffdfaca53562db02a8cb67e1774278b76b
Taiming Chen <eefozeo@gmail.com>
2019-03-24 15:23:58 +0800

    Updates

---
+++ precache-manifest.5724ca32e305c6200ba66348ade8bf53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Spotlight_Frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9449ac1b54603c2bc090",
+    "url": "/Spotlight_Frontend/static/js/runtime~main.e6f4d450.js"
+  },
+  {
+    "revision": "a49c2dfa2d9cb12f67c6",
+    "url": "/Spotlight_Frontend/static/js/main.360d8eb6.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/Spotlight_Frontend/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "a49c2dfa2d9cb12f67c6",
+    "url": "/Spotlight_Frontend/static/css/main.be8752c4.chunk.css"
+  },
+  {
+    "revision": "5eb0daa8b2fbdeb9bfe153540a51e2db",
+    "url": "/Spotlight_Frontend/index.html"
+  }
+];
