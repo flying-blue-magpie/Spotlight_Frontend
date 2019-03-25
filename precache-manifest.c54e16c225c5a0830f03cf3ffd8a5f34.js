69a38197e44c6b545440a598d487a2c41b40ee26
ci-build <ci-build@flying-blue-magpie.com>
2019-03-25 15:00:46 +0000

    Updates

---
+++ precache-manifest.c54e16c225c5a0830f03cf3ffd8a5f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a57ad621a08309f0c6",
+    "url": "/Spotlight_Frontend/static/css/main.b5d92891.chunk.css"
+  },
+  {
+    "revision": "77a57ad621a08309f0c6",
+    "url": "/Spotlight_Frontend/static/js/main.3819e0e9.chunk.js"
+  },
+  {
+    "revision": "9449ac1b54603c2bc090",
+    "url": "/Spotlight_Frontend/static/js/runtime~main.e6f4d450.js"
+  },
+  {
+    "revision": "f2be19c1cb545f6771ff",
+    "url": "/Spotlight_Frontend/static/js/2.9e492c39.chunk.js"
+  },
+  {
+    "revision": "81727782ee8be76b51c984994d3d41e4",
+    "url": "/Spotlight_Frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Spotlight_Frontend/precache-manifest.714c6a49623772baae5d802b7e24d324.js"
+  "/Spotlight_Frontend/precache-manifest.c54e16c225c5a0830f03cf3ffd8a5f34.js"
 );
 
 workbox.clientsClaim();
