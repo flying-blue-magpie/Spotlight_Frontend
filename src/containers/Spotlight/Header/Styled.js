952e73095b9f46d8be52440e8a9af8e5806c9867
Taiming Chen <eefozeo@gmail.com>
2019-03-26 07:22:53 +0800

    [refactor] modify header style

---
+++ src/containers/Spotlight/Header/Styled.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  font-size: 20px;
+
+  .header-container__icon-wrapper {
+    position: absolute;
+    right: 0;
+    margin: 0px 6px;
+  }
+  .icon-style {
+    color: white;
+    margin: 0px 8px;
+    cursor: pointer;
+    font-size: 18px;
+  }
+  .icon-transition {
+    transform: rotate(90deg);
+  }
+`;
