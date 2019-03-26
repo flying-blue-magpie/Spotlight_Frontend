02462e726279f4fb25bd2a30ae3ba1c6b284a278
Taiming Chen <eefozeo@gmail.com>
2019-03-26 20:17:28 +0800

    [feature] add DateTabsWrapper

---
+++ src/containers/Spotlight/Content/PlanningPage/EditPlanningPage/Styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const EditPlanningPageContainer = styled.div`
+  .edit-planning__cover {
+    background: lightgray;
+    height: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 8px 16px;
+    .edit-planning__cover-title {
+      font-size: 18px;
+      padding: 10px 0px;
+    }
+    .edit-planning__cover-period {
+      font-size: 12px;
+    }
+    .icon__pen {
+      font-size: 14px;
+      margin: 0px 10px;
+    }
+  }
+`;
+
+export const DateTabsWrapper = styled.div`
+  background: lightgray;
+  height: 42px;
+`;
--- src/containers/Spotlight/Content/PlanningPage/EditPlanningPage/index.js
+++ src/containers/Spotlight/Content/PlanningPage/EditPlanningPage/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+  EditPlanningPageContainer,
+  DateTabsWrapper,
+} from './Styled';
 
 const EditPlanningPage = () => (
-  <div>EditPlanningPage</div>
+  <EditPlanningPageContainer>
+    <div className="edit-planning__cover">
+      <div className="edit-planning__cover-title">
+        <span>台北購物之旅</span>
+        <i className="fas fa-pen icon__pen" />
+      </div>
+      <div className="edit-planning__cover-period">2019年6月5日-2019年6月9日 / 4天</div>
+    </div>
+    <DateTabsWrapper>
+      date tabs
+    </DateTabsWrapper>
+    <div>
+      body
+    </div>
+  </EditPlanningPageContainer>
 );
 
 export default EditPlanningPage;
