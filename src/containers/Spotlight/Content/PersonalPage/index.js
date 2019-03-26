0c8c59be45e66c9590122566b5ac0eec963d2d70
wanlinlin <leo12345639@gmail.com>
2019-03-26 20:46:19 +0800

    [feature] Add 登出 link on /我的

--- src/containers/Spotlight/Content/PersonalPage/index.js
+++ src/containers/Spotlight/Content/PersonalPage/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const PersonalPage = () => (
-  <div>PersonalPage</div>
+  <div>
+    <Link to="/">登出</Link>
+  </div>
 );
 
 export default PersonalPage;
