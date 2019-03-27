6edb5ad04e0469a6255a0c20d953defe56b8a95b
Taiming Chen <eefozeo@gmail.com>
2019-03-27 14:17:58 +0800

    [feature] modify style of cover image in TravelCard

--- src/containers/Spotlight/Content/TravelWallPage/components/TravelCard/index.js
+++ src/containers/Spotlight/Content/TravelWallPage/components/TravelCard/index.js
@@ -29,14 +29,25 @@ const StyledTravelCard = styled.div`
     flex-direction: column;
     justify-content: flex-end;
     height: 120px;
-    padding: 10px;
-    background: #C6C6C6;
+    background: black;
     color: white;
-    .travel-card__body-title {
-      font-size: 16px;
+    position: relative;
+    .travel-card__body-cover {
+      opacity: 0.5;
+      background-image: url(${(props) => props.imagePath});
+      background-size: cover;
+      height: 100%;
     }
-    .travel-card__body-period {
-      font-size: 12px;
+    .travel-card__body-text-wrapper {
+      position: absolute;
+      padding: 10px;
+      .travel-card__body-title {
+        font-size: 16px;
+        color: white;
+      }
+      .travel-card__body-period {
+        font-size: 12px;
+      }
     }
   }
   .travel-card__footer-wrapper {
@@ -56,24 +67,30 @@ const StyledTravelCard = styled.div`
   }
 `;
 
-const TravelCard = () => (
-  <StyledTravelCard>
-    <div className="travel-card__header-wrapper">
-      <i className="fas fa-user-circle travel-card__header-image" />
-      <div className="travel-card__profile-wrapper">
-        <div className="travel-card__header-name">這裡是名字</div>
-        <div className="travel-card__header-time">1天前</div>
+const TravelCard = () => {
+  const imagePath = "https://www.silverkris.com/wp-content/uploads/2017/08/taipei-ximending.jpg";
+  return (
+    <StyledTravelCard imagePath={imagePath}>
+      <div className="travel-card__header-wrapper">
+        <i className="fas fa-user-circle travel-card__header-image" />
+        <div className="travel-card__profile-wrapper">
+          <div className="travel-card__header-name">這裡是名字</div>
+          <div className="travel-card__header-time">1天前</div>
+        </div>
       </div>
-    </div>
-    <div className="travel-card__body-wrapper">
-      <div className="travel-card__body-title">台北購物之旅</div>
-      <div className="travel-card__body-period">2019年6月5日-2019年6月9日 / 4天</div>
-    </div>
-    <div className="travel-card__footer-wrapper">
-      <i className="far fa-heart travel-card__footer-icon" />
-      <div className="travel-card__footer-number">666</div>
-    </div>
-  </StyledTravelCard>
-);
+      <div className="travel-card__body-wrapper">
+        <div className="travel-card__body-cover" />
+        <div className="travel-card__body-text-wrapper">
+          <div className="travel-card__body-title">台北購物之旅</div>
+          <div className="travel-card__body-period">2019年6月5日-2019年6月9日 / 4天</div>
+        </div>
+      </div>
+      <div className="travel-card__footer-wrapper">
+        <i className="far fa-heart travel-card__footer-icon" />
+        <div className="travel-card__footer-number">666</div>
+      </div>
+    </StyledTravelCard>
+  );
+};
 
 export default TravelCard;
