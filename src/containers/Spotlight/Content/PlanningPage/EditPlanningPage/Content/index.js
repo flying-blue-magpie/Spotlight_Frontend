6b7908531e269113d11f3e09ad76586fa77d5e19
Taiming Chen <eefozeo@gmail.com>
2019-03-27 12:04:04 +0800

    [feature] add spot card

--- src/containers/Spotlight/Content/PlanningPage/EditPlanningPage/Content/index.js
+++ src/containers/Spotlight/Content/PlanningPage/EditPlanningPage/Content/index.js
@@ -1,4 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+import { fromJS } from 'immutable';
+import SpotCard from './SpotCard';
+
+const StyledContent = styled.div`
+  padding: 15px 30px;
+  .content__title {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+  .content__title-day {
+    font-size: 16px
+  }
+  .content__title-date {
+    font-size: 12px;
+  }
+`;
+
+const mockSpotData = fromJS([
+  {
+    id: 0,
+    name: '台北101',
+    address: '110台北市信義區信義路五段7號',
+    startTime: '09:00',
+  },
+  {
+    id: 1,
+    name: '台北101',
+    address: '110台北市信義區信義路五段7號',
+    startTime: '09:00',
+  },
+  {
+    id: 2,
+    name: '台北101',
+    address: '110台北市信義區信義路五段7號',
+    startTime: '09:00',
+  },
+  {
+    id: 3,
+    name: '台北101',
+    address: '110台北市信義區信義路五段7號',
+    startTime: '09:00',
+  },
+]);
 
 const Content = (props) => {
   const {
@@ -7,7 +52,20 @@ const Content = (props) => {
   const searchParams = new URLSearchParams(location.search);
   const day = searchParams.get('day');
   return (
-    <div>Day{day}</div>
+    <StyledContent>
+      <div className="content__title">
+        <div className="content__title-day">{`第${day}天`}</div>
+        <div className="content__title-date">{`2019年6月${day}日`}</div>
+      </div>
+      {
+        mockSpotData.map((spot) => (
+          <SpotCard
+            key={spot.get('id')}
+            spot={spot}
+          />
+        ))
+      }
+    </StyledContent>
   );
 };
 
