5c6a863c7d6c9523996980c02d1060208736f7c3
wanlinlin <leo12345639@gmail.com>
2019-03-30 22:32:05 +0800

    [feature] Hide Navigation on LoginPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router';
 import Spotlight from 'containers/Spotlight/';
+import history from './utils/history';
 import './App.css';
 
-const App = () => <Spotlight />;
+const App = () => (
+  <Router history={history}>
+    <Spotlight />
+  </Router>
+);
 
 export default App;
