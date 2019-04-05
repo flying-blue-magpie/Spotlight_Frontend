7c47d73af00b39944735e854c8c5a00470f71993
Taiming Chen <eefozeo@gmail.com>
2019-04-05 18:29:21 +0800

    [feature] add click function to updating icon

--- src/containers/Spotlight/Header/PlanningPage/UpdatePlanningPage/index.js
+++ src/containers/Spotlight/Header/PlanningPage/UpdatePlanningPage/index.js
@@ -1,5 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
 import { PAGE_NAME } from 'Styled/Settings/constants';
+import history from 'utils/history';
 import {
   HeaderContainer,
 } from 'containers/Spotlight/Header/Styled';
@@ -7,7 +9,7 @@ import Context from 'containers/Spotlight/Context';
 
 const { SpotlightContext } = Context;
 
-const UpdatePlanningPage = () => {
+const UpdatePlanningPage = (props) => {
   const context = useContext(SpotlightContext);
   const {
     setIsNavVisible,
@@ -18,12 +20,31 @@ const UpdatePlanningPage = () => {
       setIsNavVisible(true);
     };
   }, []);
+  const handleGoBackToDetailPlanning = useCallback(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const day = searchParams.get('day');
+    const { projectId } = props.match.params;
+    history.push({
+      pathname: `/${PAGE_NAME.DETAIL_PLANNING}/${projectId}`,
+      search: `?day=${day}`,
+    });
+  }, []);
+  const handleOnCheckBtn = useCallback(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const day = searchParams.get('day');
+    const { projectId } = props.match.params;
+    history.push({
+      pathname: `/${PAGE_NAME.DETAIL_PLANNING}/${projectId}`,
+      search: `?day=${day}`,
+    });
+  }, []);
   return (
     <HeaderContainer>
       <div className="header-container__icon-wrapper icon-left">
         <i
           role="presentation"
           className="fas fa-times icon-style"
+          onClick={handleGoBackToDetailPlanning}
         />
       </div>
       <div>{PAGE_NAME.UPDATE_PLANNING}</div>
@@ -31,10 +52,19 @@ const UpdatePlanningPage = () => {
         <i
           role="presentation"
           className="fas fa-check icon-style"
+          onClick={handleOnCheckBtn}
         />
       </div>
     </HeaderContainer>
   );
+};
+
+UpdatePlanningPage.propTypes = {
+  match: PropTypes.object,
+};
+
+UpdatePlanningPage.defaultProps = {
+  match: {},
 };
 
 export default UpdatePlanningPage;
