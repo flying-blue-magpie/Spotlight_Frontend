48a2293ff8e4abc03e0877da7ce06327737c05c1
wanlinlin <leo12345639@gmail.com>
2019-04-06 00:22:31 +0800

    [feature] Submit updateProject on AddSpotToProjectPlanPage check button click

--- src/containers/Spotlight/Header/AddSpotToProjectPlanPage/index.js
+++ src/containers/Spotlight/Header/AddSpotToProjectPlanPage/index.js
@@ -1,7 +1,10 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import history from 'utils/history';
 import Context from 'containers/Spotlight/Context';
+import { submitUpdateProject } from 'containers/Spotlight/actions';
 import {
   HeaderContainer,
   HeaderLeftButtons,
@@ -12,8 +15,11 @@ import {
 
 const { SpotlightContext } = Context;
 
-const AddSpotToProjectPlanPage = () => {
-  const { setIsNavVisible } = useContext(SpotlightContext);
+const AddSpotToProjectPlanPage = ({
+  handleSubmitUpdateProject,
+  match,
+}) => {
+  const { setIsNavVisible, updateProject } = useContext(SpotlightContext);
 
   useEffect(() => {
     setIsNavVisible(false);
@@ -21,6 +27,11 @@ const AddSpotToProjectPlanPage = () => {
       setIsNavVisible(true);
     };
   }, []);
+
+  const handleOnCheckClick = useCallback(() => {
+    history.goBack();
+    handleSubmitUpdateProject(match.params.projectId, updateProject);
+  });
 
   return (
     <HeaderContainer>
@@ -33,7 +44,7 @@ const AddSpotToProjectPlanPage = () => {
         加到我的行程
       </HeaderTitle>
       <HeaderRightButtons>
-        <HeaderButton type="button" onClick={() => history.goBack()}>
+        <HeaderButton type="button" onClick={handleOnCheckClick}>
           <i className="fas fa-check" />
         </HeaderButton>
       </HeaderRightButtons>
@@ -42,6 +53,14 @@ const AddSpotToProjectPlanPage = () => {
 };
 
 AddSpotToProjectPlanPage.propTypes = {
+  handleSubmitUpdateProject: PropTypes.func.isRequired,
+  match: PropTypes.object.isRequired,
 };
 
-export default withRouter(AddSpotToProjectPlanPage);
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmitUpdateProject: (projectId, updatedProject) => (
+    dispatch(submitUpdateProject(projectId, updatedProject))
+  ),
+});
+
+export default withRouter(connect(null, mapDispatchToProps)(AddSpotToProjectPlanPage));
