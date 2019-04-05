dcd0db69918b181d122a785f949bee15a3b5e8eb
Taiming Chen <eefozeo@gmail.com>
2019-04-05 18:24:05 +0800

    [feature] add updating icon to udpate page

--- src/containers/Spotlight/Header/PlanningPage/UpdatePlanningPage/index.js
+++ src/containers/Spotlight/Header/PlanningPage/UpdatePlanningPage/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { PAGE_NAME } from 'Styled/Settings/constants';
 import {
   HeaderContainer,
 } from 'containers/Spotlight/Header/Styled';
+import Context from 'containers/Spotlight/Context';
 
-export default () => (
-  <HeaderContainer>
-    {PAGE_NAME.UPDATE_PLANNING}
-  </HeaderContainer>
-);
+const { SpotlightContext } = Context;
+
+const UpdatePlanningPage = () => {
+  const context = useContext(SpotlightContext);
+  const {
+    setIsNavVisible,
+  } = context;
+  useEffect(() => {
+    setIsNavVisible(false);
+    return () => {
+      setIsNavVisible(true);
+    };
+  }, []);
+  return (
+    <HeaderContainer>
+      <div className="header-container__icon-wrapper icon-left">
+        <i
+          role="presentation"
+          className="fas fa-times icon-style"
+        />
+      </div>
+      <div>{PAGE_NAME.UPDATE_PLANNING}</div>
+      <div className="header-container__icon-wrapper icon-right">
+        <i
+          role="presentation"
+          className="fas fa-check icon-style"
+        />
+      </div>
+    </HeaderContainer>
+  );
+};
+
+export default UpdatePlanningPage;
