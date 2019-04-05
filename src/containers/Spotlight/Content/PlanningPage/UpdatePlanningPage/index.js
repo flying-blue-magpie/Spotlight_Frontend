d85221af8015f1d27d83685a3f03c2fe5d92b2e9
Taiming Chen <eefozeo@gmail.com>
2019-04-05 19:41:09 +0800

    [feature] add inputbox to updating page

--- src/containers/Spotlight/Content/PlanningPage/UpdatePlanningPage/index.js
+++ src/containers/Spotlight/Content/PlanningPage/UpdatePlanningPage/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import InputBox from 'components/InputBox';
+
+const UpdatePlanningPageWrapper = styled.div`
+  padding: 0px 15px;
+`;
 
 const UpdatePlanningPage = () => (
-  <div>UpdatePlanningPage</div>
+  <UpdatePlanningPageWrapper>
+    <InputBox
+      title="旅程名稱"
+      handleOnChange={() => {}}
+    />
+    <InputBox
+      title="出發日期"
+      inputType="date"
+      handleOnChange={() => {}}
+    />
+  </UpdatePlanningPageWrapper>
 );
 
 export default UpdatePlanningPage;
