0add09cd581b5b97830c7f2923254633600c0e60
Taiming Chen <eefozeo@gmail.com>
2019-04-06 18:22:07 +0800

    [feature] styling InputBox

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -4,24 +4,31 @@ import styled from 'styled-components';
 
 const InputBoxWrapper = styled.div`
   display: flex;
-  border: 1px solid #B7B7B7;
   height: 40px;
   color: #707070;
   margin-top: 15px;
+  border-radius: 20px;
   .input-box__label-box {
-    background: #ececec;
+    background: rgba(238, 238, 238, 1);
     width: 85px;
     display: flex;
     align-items: center;
     justify-content: center;
     font-size: 14px;
+    color: rgba(51, 51, 51, 1);
+    font-weight: 500;
+    letter-spacing: 1px;
+    border-radius: 20px 0px 0px 20px;
+    padding-left: 10px;
   }
   .input-box__input {
+    background: rgba(238, 238, 238, 1);
     border: none;
     outline: none;
     font-size: 14px;
     padding: 0px 10px;
     flex-grow: 1;
+    border-radius: 0px 20px 20px 0px;
   }
 `;
 
