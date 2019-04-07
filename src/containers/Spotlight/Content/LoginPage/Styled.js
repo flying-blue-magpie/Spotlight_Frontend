499791629731a8c324848884f279717cda1d7033
wanlinlin <leo12345639@gmail.com>
2019-04-07 22:07:53 +0800

    [feature] Update LoginPage styles

--- src/containers/Spotlight/Content/LoginPage/Styled.js
+++ src/containers/Spotlight/Content/LoginPage/Styled.js
@@ -11,32 +11,99 @@ export const Container = styled.div`
   min-height: 100%;
 `;
 
+const inputLeftPadding = '56px';
+
+export const UserNameContainer = styled.div`
+  position: relative;
+  margin: 0 15px 10px;
+
+  &:before {
+    font-family: "Font Awesome 5 Free";
+    content: "\f007";
+    display: inline-block;
+    vertical-align: middle;
+    font-weight: 900;
+    position: absolute;
+    top: 50%;
+    left: calc(${inputLeftPadding} / 2);
+    transform: translate(-50%, -50%);
+  }
+`;
+
 export const UserName = styled.input`
-  ${row}
-  margin-top: 24px;
-  border: solid 1px lightgray;
-  font-size: 16px;
+  width: 100%;
+  border: 0;
+  font-size: 14px;
+  border-radius: 20px;
+  background-color: #EEEEEE;
+  padding: 10px 0 10px ${inputLeftPadding};
+  appearance: none;
+  outline: none;
+  color: #333;
+`;
+
+
+export const PassWordContainer = styled.div`
+  position: relative;
+  margin: 0 15px 30px;
+`;
+
+export const PasswordIcon = styled.img`
+  position: absolute;
+  top: 50%;
+  left: calc(${inputLeftPadding} / 2);
+  transform: translate(-50%, -50%);
 `;
 
 export const PassWord = styled.input`
-  ${row}
-  border: solid 1px lightgray;
-  font-size: 16px;
+  width: 100%;
+  border: 0;
+  font-size: 14px;
+  border-radius: 20px;
+  background-color: #EEEEEE;
+  padding: 10px 0 10px ${inputLeftPadding};
+  appearance: none;
+  outline: none;
+  color: #333;
 `;
 
+const loginButtonHorizontalMargin = '15px';
+
 export const LoginButton = styled.button`
-  ${row}
-  line-height: 1.5;
-  background-color: lightgray;
+  background-color: #F9D94A;
+  border-radius: 20px;
+  appearance: none;
+  border: 0;
+  margin: 0 ${loginButtonHorizontalMargin} 15px;
   font-size: 16px;
+  width: calc(100% - 2 * ${loginButtonHorizontalMargin});
+  display: block;
+  color: #333333;
+  font-weight: bold;
+  padding: 10px 0;
   cursor: pointer;
   outline: none;
 `;
 
 export const RegisterRow = styled.div`
-  ${row}
   display: flex;
   justify-content: center;
+  color: #333333;
+  font-size: 12px;
+`;
+
+export const RegisterLink = styled.a`
+  margin-left: 3px;
+  font-weight: bold;
+  
+  &,
+  &:hover,
+  &:active,
+  &:focus,
+  &:visited {
+    color: #333333;
+    text-decoration: underline;
+  }
 `;
 
 export const ErrorMessage = styled.div`
@@ -44,10 +111,9 @@ export const ErrorMessage = styled.div`
   color: red;
 `;
 
-export const Logo = styled.img`
-  width: 125px;
-  display: block;
-  margin: 0 auto;
-  border-radius: 50%;
-  margin-top: 24px;
+export const Logo = styled.div`
+  color: #333333;
+  font-size: 37px;
+  padding: 115px 0 58px;
+  text-align: center;
 `;
--- src/containers/Spotlight/Content/LoginPage/index.js
+++ src/containers/Spotlight/Content/LoginPage/index.js
@@ -24,7 +24,12 @@ import {
   ErrorMessage,
   Logo,
   Container,
+  UserNameContainer,
+  PassWordContainer,
+  PasswordIcon,
+  RegisterLink,
 } from './Styled';
+import keyIcon from './key.svg';
 
 const { SpotlightContext } = Context;
 
@@ -56,30 +61,44 @@ const Login = (props) => {
     });
   };
 
-  const { setIsHeaderVisible } = useContext(SpotlightContext);
+  const {
+    setIsHeaderVisible,
+    setIsNavVisible,
+  } = useContext(SpotlightContext);
 
   useEffect(() => {
     if (loginStatusMeta.get('isLoaded') && user) {
       history.push(`/${PAGE_NAME.EXPLORE}`);
     }
+  }, [user]);
+
+  useEffect(() => {
     setIsHeaderVisible(false);
+    setIsNavVisible(false);
 
     return () => {
       setIsHeaderVisible(true);
+      setIsNavVisible(true);
     };
-  }, [user]);
+  }, []);
 
   return (
     loginStatusMeta.get('isLoaded') && !user ? (
       <Container>
-        <Logo src="https://avatars0.githubusercontent.com/u/48876369?s=200&v=4" />
-        <UserName ref={usernameRef} type="email" placeholder="輸入電子信箱/用戶名" />
-        <PassWord ref={passwordRef} type="password" placeholder="輸入密碼" />
+        <Logo>LOGO</Logo>
+        <UserNameContainer>
+          <UserName ref={usernameRef} type="email" placeholder="請輸入註冊的 E-mail" />
+        </UserNameContainer>
+        <PassWordContainer>
+          <PasswordIcon src={keyIcon} />
+          <PassWord ref={passwordRef} type="password" placeholder="輸入密碼" />
+        </PassWordContainer>
         <LoginButton onClick={handleLoginBtnClick}>登入</LoginButton>
         <RegisterRow>
-          <a href="/#" onClick={handleRegisterBtnClick}>
-            立即註冊
-          </a>
+          還沒有帳號嗎?
+          <RegisterLink href="/#" onClick={handleRegisterBtnClick}>
+            前往註冊
+          </RegisterLink>
         </RegisterRow>
         {loginMeta.get('error') && <ErrorMessage>登入失敗</ErrorMessage>}
       </Container>
