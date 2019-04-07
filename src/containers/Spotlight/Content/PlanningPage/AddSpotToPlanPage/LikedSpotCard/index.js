d3911831834f78c7f6cd9d9dded97221e2533741
Taiming Chen <eefozeo@gmail.com>
2019-04-07 09:38:32 +0800

    [feature] install ImageGallery

--- src/containers/Spotlight/Content/PlanningPage/AddSpotToPlanPage/LikedSpotCard/index.js
+++ src/containers/Spotlight/Content/PlanningPage/AddSpotToPlanPage/LikedSpotCard/index.js
@@ -1,15 +1,52 @@
 import React, { useEffect } from 'react';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
 import Spinner from 'components/Spinner';
+import ImageGallery from 'react-image-gallery';
+
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const HEIGHT_SPOT_CARD = 140;
 
 const LikedSpotCardContainer = styled.div`
   width: 100%;
-height: ${HEIGHT_SPOT_CARD}px;
+  height: ${HEIGHT_SPOT_CARD}px;
   background: red;
+  position: relative;
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.16);
+  border-radius: 10px;
+  cursor: pointer;
+
+  .liked-spot-card__card-wrapper {
+    position: absolute;
+    width: 100%;
+    height: 100px;
+    border-radius: 10px;
+  }
+  .image-gallery-image {
+    height: 100px;
+    max-height: 100px;
+  }
+  .liked-spot-card__card-cover {
+    height: 100%;
+
+  }
+  .liked-spot-card__card-name {
+    height: 40px;
+    background: white;
+    border-radius: 0px 0px 10px 10px;
+  }
+  .liked-spot-card__mask {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #000000;
+    opacity: 0.7;
+    border: 4px solid #AAAAAA;
+    border-radius: 10px;
+  }
 `;
 
 const LikedSpotCard = ({
@@ -26,10 +63,32 @@ const LikedSpotCard = ({
   if (!foundSpotDetail) {
     return <Spinner height={HEIGHT_SPOT_CARD} />;
   }
+  const showMask = false;
+  const pics = foundSpotDetail.get('pic').map((pic) => ({
+    original: pic,
+    thumbnail: pic,
+  })).toJS();
 
   return (
     <LikedSpotCardContainer>
-      {foundSpotDetail.get('name')}
+      <div className="liked-spot-card__card-wrapper">
+        <div className="liked-spot-card__card-cover">
+          <ImageGallery
+            items={pics}
+            showThumbnails={false}
+            showFullscreenButton={false}
+            showPlayButton={false}
+            showNav={false}
+            autoPlay
+            slideInterval={getRandom(3000, 5000)}
+          />
+        </div>
+        <div className="liked-spot-card__card-name">{foundSpotDetail.get('name')}</div>
+      </div>
+      {
+        showMask &&
+        <div className="liked-spot-card__mask">Mask</div>
+      }
     </LikedSpotCardContainer>
   );
 };
@@ -44,10 +103,8 @@ LikedSpotCard.propTypes = {
 };
 
 LikedSpotCard.defaultProps = {
-  // favoriteSpotIds: List(),
-  // match: {},
   spots: Map(),
-  handleFetchSpotById: () => {},
+  handleFetchSpotById: () => { },
 };
 
 export default LikedSpotCard;
