7bfcddf593dd8488f97467f14893a583b56b8fb6
wanlinlin <leo12345639@gmail.com>
2019-04-07 20:51:47 +0800

    [feature] Update AddSpotToProjectPlanPage styles

--- src/containers/Spotlight/Content/AddSpotToProjectPlanPage/Styled.js
+++ src/containers/Spotlight/Content/AddSpotToProjectPlanPage/Styled.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  min-height: 100%;
+  background-color: #FAFAFA;
+`;
+
 export const ProjectTitle = styled.div`
   padding: 9px 0;
+  font-size: 16px;
+  color: #EEEEEE;
   text-align: center;
-  background-color: #bbb;
+  background-color: #333333;
 `;
 
 export const Plan = styled.label`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 12px;
+  padding: 10px 0;
+  margin: 0 15px;
   border-bottom: solid 1px lightgray;
+  color: #333333;
+  font-size: 14px;
 `;
