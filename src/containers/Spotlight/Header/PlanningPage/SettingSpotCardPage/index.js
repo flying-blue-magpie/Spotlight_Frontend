e05522bd979b2b2c9eefa0790007f77913fb2232
Taiming Chen <eefozeo@gmail.com>
2019-04-07 16:28:38 +0800

    [feature] add ability to go back

--- src/containers/Spotlight/Header/PlanningPage/SettingSpotCardPage/index.js
+++ src/containers/Spotlight/Header/PlanningPage/SettingSpotCardPage/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 // import PropTypes from 'prop-types';
-// import history from 'utils/history';
+import history from 'utils/history';
 import {
   HeaderContainer,
 } from 'containers/Spotlight/Header/Styled';
@@ -10,20 +10,25 @@ import { PAGE_NAME } from 'Styled/Settings/constants';
 import arrowLeftIconPath from 'assets/arrow_left_icon.svg';
 import checkIconPath from 'assets/check_icon.svg';
 
-const SettingSpotCardPage = () => (
-  <HeaderContainer>
-    <div className="header-container__icon-wrapper icon-left">
-      <div role="presentation" onClick={() => {}}>
-        <img className="icon-style" src={arrowLeftIconPath} alt="" />
+const SettingSpotCardPage = () => {
+  const handleOnGoBack = useCallback(() => {
+    history.goBack();
+  }, []);
+  return (
+    <HeaderContainer>
+      <div className="header-container__icon-wrapper icon-left">
+        <div role="presentation" onClick={handleOnGoBack}>
+          <img className="icon-style" src={arrowLeftIconPath} alt="" />
+        </div>
       </div>
-    </div>
-    <div>{PAGE_NAME.SETTING_SPOT_CARD.text}</div>
-    <div className="header-container__icon-wrapper icon-right">
-      <div role="presentation" onClick={() => {}}>
-        <img className="icon-style" src={checkIconPath} alt="" />
+      <div>{PAGE_NAME.SETTING_SPOT_CARD.text}</div>
+      <div className="header-container__icon-wrapper icon-right">
+        <div role="presentation" onClick={() => {}}>
+          <img className="icon-style" src={checkIconPath} alt="" />
+        </div>
       </div>
-    </div>
-  </HeaderContainer>
-);
+    </HeaderContainer>
+  );
+};
 
 export default SettingSpotCardPage;
