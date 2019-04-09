4386b8c7f466ce5c20893583c066f2038d245654
wanlinlin <leo12345639@gmail.com>
2019-04-09 21:04:31 +0800

    [feature] Add project data to TravelWallPage

--- src/containers/Spotlight/Content/TravelWallPage/index.js
+++ src/containers/Spotlight/Content/TravelWallPage/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { Map } from 'immutable';
 import { PAGE_NAME } from 'Styled/Settings/constants';
+import { fetchProjects } from 'containers/Spotlight/actions';
+import { selectPublicProjects } from 'containers/Spotlight/selectors';
 import { Container, TravelCard } from './Styled';
 
-const TravelWallPage = () => {
-  const cards = new Array(10).fill(0).map((x, index) => index);
-  const detailPlanningPagePath = `/${PAGE_NAME.DETAIL_PLANNING.name}`;
-  const defaultDay = 1;
+const TravelWallPage = ({
+  handleFetchProjects,
+  publicProjects,
+}) => {
+  useEffect(() => {
+    handleFetchProjects();
+  }, []);
 
   return (
     <Container>
       {
-        cards.map((card, index) => (
+        publicProjects.map((project) => (
           <TravelCard
-            key={card}
+            key={project.get('proj_id')}
             to={{
-              pathname: `${detailPlanningPagePath}/${index}`,
-              search: `?day=${defaultDay}`,
+              pathname: `/${PAGE_NAME.DETAIL_PLANNING.name}/${project.get('proj_id')}`,
+              search: '?day=1',
             }}
+            cardTitle={project.get('name')}
+            cardDate={project.get('created_time')}
+            cardSubtitle={`${moment(project.get('start_day'), 'YYYY-MM-DD').format('YYYY年MM月DD日')}-${moment(project.get('start_day'), 'YYYY-MM-DD').add(project.get('tot_days') - 1, 'days').format('YYYY年MM月DD日')} / ${project.get('tot_days')}天`}
+            likeNumber={project.get('like_num')}
           />
         ))
       }
@@ -24,4 +38,18 @@ const TravelWallPage = () => {
   );
 };
 
-export default TravelWallPage;
+TravelWallPage.propTypes = {
+  handleFetchProjects: PropTypes.func.isRequired,
+  publicProjects: PropTypes.instanceOf(Map).isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  publicProjects: selectPublicProjects(),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleFetchProjects: () => dispatch(fetchProjects({ onlyPublic: true })),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TravelWallPage);
