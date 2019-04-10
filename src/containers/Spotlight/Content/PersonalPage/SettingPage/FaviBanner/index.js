45aa2ad5d83edfc37726d99a7b30fe8f23fab71b
Boxi <boxi800108@gmail.com>
2019-04-10 22:38:51 +0800

    [feature] Add psersonal image for setting page

--- src/containers/Spotlight/Content/PersonalPage/SettingPage/FaviBanner/index.js
+++ src/containers/Spotlight/Content/PersonalPage/SettingPage/FaviBanner/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { PAGE_NAME } from 'Styled/Settings/constants';
+import PropTypes from 'prop-types';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { Map } from 'immutable';
+import { selectUser } from 'containers/Spotlight/selectors';
 import { StyledFaviBanner } from './styles';
 
-const FaviBanner = () => (
-  <StyledFaviBanner
-    to={`/${PAGE_NAME.SETTING_INFO.name}`}
-  >
-    <div className="favi-banner__favicon-container">
-      <div className="favi-banner__favicon" />
-    </div>
-    <div className="favi-banner__favicon-right-arrow" />
-  </StyledFaviBanner>
-);
+const FaviBanner = ({
+  user,
+}) => {
+  const faviconPath = (user && user.get('protrait')) || 'http://i.imgur.com/EUAd2ht.jpg';
+  return (
+    <StyledFaviBanner
+      faviconPath={faviconPath}
+    >
+      <div className="favi-banner__favicon-container">
+        <div className="favi-banner__favicon" />
+      </div>
+      <div className="favi-banner__favicon-right-arrow" />
+    </StyledFaviBanner>
+  );
+};
 
-export default FaviBanner;
+FaviBanner.propTypes = {
+  user: PropTypes.instanceOf(Map),
+};
+
+const mapStateToProps = createStructuredSelector({
+  user: selectUser(),
+});
+
+export default connect(mapStateToProps)(FaviBanner);
