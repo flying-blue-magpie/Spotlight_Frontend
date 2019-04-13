442e93cd9c8fff073ad8e49e2b64832dba7c12f1
wanlinlin <leo12345639@gmail.com>
2019-04-13 12:56:16 +0800

    [feature] Handle search rec spots actions

--- src/containers/Spotlight/reducer.js
+++ src/containers/Spotlight/reducer.js
@@ -63,12 +63,15 @@ import {
   SET_CANCEL_LIKE_PROJECT_DONE,
   SET_REC_SPOTS_LOADING,
   SET_REC_SPOTS_DONE,
+  SET_SEARCH_REC_SPOTS_LOADING,
+  SET_SEARCH_REC_SPOTS_DONE,
 } from './constants';
 
 const initialState = fromJS({
   setSpotMeta: META,
   setSpotsMeta: META,
   setRecSpotsMeta: META,
+  setSearchRecSpotsMeta: META,
   spots: {},
   spotsResult: [],
   recSpotsResult: [],
@@ -226,8 +229,34 @@ function spotLightReducer(state = initialState, action) {
             recSpotsResult,
           ),
         )
-        .update('setRecSpotsMeta', updateMetaDone)
-        // only update exploring spot id for initial value
+        .update('setRecSpotsMeta', updateMetaDone);
+    }
+
+    case SET_SEARCH_REC_SPOTS_LOADING:
+      return state
+        .update('setSearchRecSpotsMeta', updateMetaLoading)
+        .set('recSpotsResult', initialState.get('recSpotsResult'))
+        .set('exploringSpotId', null);
+
+    case SET_SEARCH_REC_SPOTS_DONE: {
+      const {
+        error,
+        entities,
+        result,
+      } = action.payload;
+      if (error) {
+        return state.update('setSearchRecSpotsMeta', updateMetaError);
+      }
+      return state
+        .mergeDeepIn(['spots'], fromJS(entities.spots))
+        .update(
+          'recSpotsResult',
+          (recSpotsResult) => result.reduce(
+            (ids, id) => (ids.indexOf(id) !== -1 ? ids : ids.push(id)),
+            recSpotsResult,
+          ),
+        )
+        .update('setSearchRecSpotsMeta', updateMetaDone)
         .update('exploringSpotId', (id) => (id === null ? result[0] : id));
     }
 
