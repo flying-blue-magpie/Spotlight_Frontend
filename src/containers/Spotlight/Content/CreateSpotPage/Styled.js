4845c0624464a17ea6e04b9025865d59c3cedaf6
wanlinlin <leo12345639@gmail.com>
2019-04-14 11:47:03 +0800

    [feature] Add CreateSpot page cover

---
+++ src/containers/Spotlight/Content/CreateSpotPage/Styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+`;
+
+export const Cover = styled.div`
+  height: 200px;
+  background-color: #EEEEEE;
+  color: #707070;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  padding-top: 43px;
+`;
+
+export const DefaultImage = styled.img`
+  display: block;
+  margin: 0 auto 15px;
+`;
+
+export const UploadImageButton = styled.button`
+  background-color: #fff;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 15px 0 auto;
+`;
--- src/containers/Spotlight/Content/CreateSpotPage/index.js
+++ src/containers/Spotlight/Content/CreateSpotPage/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import {
+  Container,
+  Cover,
+  DefaultImage,
+  UploadImageButton,
+} from './Styled';
+import defaultImageSrc from './default-image.svg';
+import cameraImageSrc from './camera.svg';
 
-const CreateSpotPage = () => <div>create spot page</div>;
+const CreateSpotPage = () => (
+  <Container>
+    <Cover>
+      <DefaultImage src={defaultImageSrc} />
+      請上傳景點照片
+      <UploadImageButton>
+        <img src={cameraImageSrc} alt="上傳照片" />
+      </UploadImageButton>
+    </Cover>
+  </Container>
+);
 
 export default CreateSpotPage;
