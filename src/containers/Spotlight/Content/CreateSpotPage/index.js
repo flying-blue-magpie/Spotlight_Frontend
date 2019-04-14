696c66a1351394da9aa896ede6ee8e822c7eef3b
wanlinlin <leo12345639@gmail.com>
2019-04-14 12:09:46 +0800

    [feature] Hide header and nav on CreateSpot page

--- src/containers/Spotlight/Content/CreateSpotPage/index.js
+++ src/containers/Spotlight/Content/CreateSpotPage/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import Context from 'containers/Spotlight/Context';
 import {
   Container,
   Cover,
@@ -13,41 +14,56 @@ import {
 import defaultImageSrc from './default-image.svg';
 import cameraImageSrc from './camera.svg';
 
-const CreateSpotPage = () => (
-  <Container>
-    <Cover>
-      <DefaultImage src={defaultImageSrc} />
-      請上傳景點照片
-      <UploadImageButton>
-        <img src={cameraImageSrc} alt="上傳照片" />
-      </UploadImageButton>
-    </Cover>
-    <Fields>
-      <Field>
-        <FieldName>景點名稱</FieldName>
-        <TextInput placeholder="請輸入景點名稱" />
-      </Field>
-      <Field>
-        <FieldName>景點地區</FieldName>
-      </Field>
-      <Field>
-        <FieldName>景點地址</FieldName>
-        <TextInput placeholder="請輸入具體地址" />
-      </Field>
-      <Field>
-        <FieldName>電話</FieldName>
-        <TextInput />
-      </Field>
-      <Field>
-        <FieldName>網址</FieldName>
-        <TextInput />
-      </Field>
-      <Field>
-        <FieldName>景點備註</FieldName>
-        <TextArea placeholder="可以寫上與景點有關資訊。例如遊玩方式、交通轉乘資訊或是景點相關簡介等等。" />
-      </Field>
-    </Fields>
-  </Container>
-);
+const { SpotlightContext } = Context;
+
+const CreateSpotPage = () => {
+  const { setIsHeaderVisible, setIsNavVisible } = useContext(SpotlightContext);
+  useEffect(() => {
+    setIsHeaderVisible(false);
+    setIsNavVisible(false);
+
+    return () => {
+      setIsHeaderVisible(true);
+      setIsNavVisible(true);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <Cover>
+        <DefaultImage src={defaultImageSrc} />
+        請上傳景點照片
+        <UploadImageButton>
+          <img src={cameraImageSrc} alt="上傳照片" />
+        </UploadImageButton>
+      </Cover>
+      <Fields>
+        <Field>
+          <FieldName>景點名稱</FieldName>
+          <TextInput placeholder="請輸入景點名稱" />
+        </Field>
+        <Field>
+          <FieldName>景點地區</FieldName>
+        </Field>
+        <Field>
+          <FieldName>景點地址</FieldName>
+          <TextInput placeholder="請輸入具體地址" />
+        </Field>
+        <Field>
+          <FieldName>電話</FieldName>
+          <TextInput />
+        </Field>
+        <Field>
+          <FieldName>網址</FieldName>
+          <TextInput />
+        </Field>
+        <Field>
+          <FieldName>景點備註</FieldName>
+          <TextArea placeholder="可以寫上與景點有關資訊。例如遊玩方式、交通轉乘資訊或是景點相關簡介等等。" />
+        </Field>
+      </Fields>
+    </Container>
+  );
+};
 
 export default CreateSpotPage;
