56fa94b1f8bc430a4501c899fe8893b85089db8e
wanlinlin <leo12345639@gmail.com>
2019-04-14 12:05:42 +0800

    [feature] Add CreateSpot input fields

--- src/containers/Spotlight/Content/CreateSpotPage/Styled.js
+++ src/containers/Spotlight/Content/CreateSpotPage/Styled.js
@@ -28,3 +28,49 @@ export const UploadImageButton = styled.button`
   justify-content: center;
   margin: 0 15px 0 auto;
 `;
+
+export const Fields = styled.div`
+  padding: 15px;
+`;
+
+export const Field = styled.div`
+  border-radius: 20px;
+  background-color: #EEEEEE;
+  padding: 10px 0;
+  display: flex;
+
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+
+export const FieldName = styled.div`
+  color: #333333;
+  font-size: 14px;
+  font-weight: bold;
+  flex-basis: 25%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextInput = styled.input`
+  color: #AAAAAA;
+  font-size: 14px;
+  background-color: transparent;
+  appearance: none;
+  outline: none;
+  border: 0;
+  flex-basis: 75%;
+`;
+
+export const TextArea = styled.textarea`
+  color: #AAAAAA;
+  font-size: 14px;
+  background-color: transparent;
+  appearance: none;
+  outline: none;
+  border: 0;
+  height: 78px;
+  flex-basis: 75%;
+`;
