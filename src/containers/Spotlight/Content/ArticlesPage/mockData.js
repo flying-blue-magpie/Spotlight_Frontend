3c1935559bf648d3a6595db5c27ee4a1cfb83b29
Taiming Chen <taiming@bridgewell.com>
2019-05-20 00:02:09 +0800

    add articles card and content

---
+++ src/containers/Spotlight/Content/ArticlesPage/mockData.js
@@ -0,0 +1,30 @@
+import imageBuilding from 'containers/Spotlight/Content/ArticlesPage/photo/buildings-1846728_640@3x.png';
+import imageSleepCat from 'containers/Spotlight/Content/ArticlesPage/photo/taiwan-667563_640@3x.png';
+
+import imageYehliu from 'containers/Spotlight/Content/ArticlesPage/photo/yehliu-geopark-257886_640@3x.png';
+import imageTofu from 'containers/Spotlight/Content/ArticlesPage/photo/taiwan-1057813_640@3x.png';
+import imageCow from 'containers/Spotlight/Content/ArticlesPage/photo/chingtienkang-1784872_640@3x.png';
+
+export const articles = [
+  {
+    id: 0,
+    title: '象山好風景！',
+    content: '今天是美好的一天，早晨朦朧的陽光灑在青青的草地上，彷彿綠色畫布上油墨的驚然一撇。',
+    timestamp: '2019/05/11 15:26',
+    images: [
+      imageBuilding,
+      imageSleepCat,
+    ],
+  },
+  {
+    id: 1,
+    title: '野柳 - 郊外踏青好去處',
+    content: '野柳是北海岸的著名觀光景點之一，現在的規劃也是非常好,趁著假日天氣好來這邊玩，私密景點跟美食這邊偷偷告訴你...',
+    timestamp: '2019/04/28 11:54',
+    images: [
+      imageYehliu,
+      imageTofu,
+      imageCow,
+    ],
+  },
+];
